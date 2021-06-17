cfe267f1ee08e7317373244051e50ca639a5f52c
EC2 Default User <ec2-user@ip-172-31-65-178.ec2.internal>
2021-06-17 09:33:59 +0000

    [Add]device admin追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :customers
   root to: "public/homes#top"
   get "/about" => "public/homes#about", as: 'about'
 
@@ -8,8 +9,22 @@ Rails.application.routes.draw do
   namespace :public do
   end
 
-  # 管理者側ルーティング
-  namespace :admin do
-  end
+ # 管理者側ルーティング ヤマタツ追記
+   devise_for :admin, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
 
+  namespace :admin do
+    resources :products, only: [:index, :show, :new, :create, :edit, :update] #商品情報
+    resources :customers, only: [:index, :show, :edit, :update]               #ユーザー情報
+    resources :genres, only: [:index, :create, :edit, :update]                #ジャンル
+    resources :orders, only: [:index, :show, :update]                         #注文
+    resources :order_products, only: [:update]                                #制作ステータスを更新
+    get '/admins' => 'admins#top'                                             #管理者のトップ画面
+    get 'search' => 'products#search'
+  end
+  # 管理者側ルーティング
+  
 end
