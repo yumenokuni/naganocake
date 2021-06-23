e59a75dd716f276d1ab90c3873a27e93c6fd6b7f
miwa0828 <tatch.828@me.com>
2021-06-23 09:40:31 +0000

    ジャンル、配送先

--- db/schema.rb
+++ db/schema.rb
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_052802) do
     t.string "postal_code"
     t.string "address"
     t.datetime "created_at", null: false
+    t.datetime "update_at"
     t.datetime "updated_at", null: false
   end
 
