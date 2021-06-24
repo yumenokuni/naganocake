3dabb8c4519f9ef8839d945d05d1a6c6b052329b
miwa0828 <tatch.828@me.com>
2021-06-24 09:06:26 +0000

    削除機能の修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -15,12 +15,20 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.find(params[:id])
   end
 
-  
+
   def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params)
     redirect_to admin_genres_path
   end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to admin_genres_path
+  end
+
+
 
   private
   def genre_params
