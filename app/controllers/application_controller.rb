67b7227e9228fe681f34d16eb2b6ebdc66035e38
Aya-so <ayaka.wakame0315@gmail.com>
2021-06-27 07:51:33 +0000

    [Fix]注文一覧遷移先修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
       when Admin
-        admin_products_path
+        admin_orders_path
       when Customer
         root_path
     end
